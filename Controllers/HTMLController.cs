9722fd4403ffe4c34efba6126a4f6190534a6364
tranhaaaaaa <cauchunso2002@gmail.com>
2023-06-05 23:51:08 +0700

    first commit

---
+++ Controllers/HTMLController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAzure.Controllers
+{
+    public class HTMLController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult AppleTouchIcon()
+        {
+            return View("~/Views/HTML/apple-touch-icon.cshtml");
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult LoginRegister()
+        {
+            return View("~/Views/HTML/login-register.cshtml");
+        }
+        public IActionResult ProductDetails()
+        {
+            return View("~/Views/HTML/product-details.cshtml");
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult ShopSideBar()
+        {
+            return View("~/Views/HTML/shop-sidebar.cshtml");
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult WishList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
